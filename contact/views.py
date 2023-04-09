c2726075f646ce61802b9ff6e17585f8a2702391
Rafael Ortiz <rortiz@inkremental.co>
2023-04-09 15:43:22 -0500

    Get data from form inputs

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
 
 
 def contact(request):
     form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(data=request.POST)
+        if form.is_valid():
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            message = request.POST.get('message')
+            return redirect('/contact/?valid')
+            
     return render(request, 'contact/contact.html', {'form': form})
