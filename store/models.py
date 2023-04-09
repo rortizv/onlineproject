2ba6a8421a2db641f1648fa37c1a4e3f2243cf9a
Rafael Ortiz <rortiz@inkremental.co>
2023-04-09 17:42:33 -0500

    Migrate store models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'productCategory'
+        verbose_name_plural = 'productCategories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    categories = models.ForeignKey('ProductCategory', on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True, upload_to='store')
+    price = models.FloatField()
+    availability = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.name
