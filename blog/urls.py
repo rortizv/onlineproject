b4c06a93f346c1f69b838af250f29d260c7bf456
Rafael Ortiz <rortiz@inkremental.co>
2023-04-09 13:17:56 -0500

    Filter by category

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.posts, name='Blog'),
+    path('category/<int:category_id>/', views.category, name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Category
 
 
 def posts(request):
     # Get all posts from the database
     posts = Post.objects.all()
     return render(request, 'blog/blog.html', {'posts': posts})
+
+
+def category(request, category_id):
+    # Get all posts from the database filtered by category_id
+    category = Category.objects.get(id=category_id)
+    posts = Post.objects.filter(categories = category)
+    return render(request, 'blog/categories.html', {'category': category, 'posts': posts})
