bde0330a3afab4abfcdf3112adf7c7c692d0f26b
Rafael Ortiz <rortiz@inkremental.co>
2023-04-09 13:53:12 -0500

    Filter by category

--- blog/views.py
+++ blog/views.py
@@ -12,4 +12,5 @@ def category(request, category_id):
     # Get all posts from the database filtered by category_id
     category = Category.objects.get(id=category_id)
     posts = Post.objects.filter(categories = category)
+    print(posts.values())
     return render(request, 'blog/categories.html', {'category': category, 'posts': posts})
