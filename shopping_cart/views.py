7eb8d649b6ae3132708c89ba43cea62dfb7674f1
Rafael Ortiz <rortiz@inkremental.co>
2023-04-10 20:37:24 -0500

    Add to cart button

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .shopping_cart import ShoppingCart
+from store.models import Product
 
-# Create your views here.
+
+def add_product(request, product_id):
+    shopping_cart = ShoppingCart(request)
+    product = Product.objects.get(id=product_id)
+    shopping_cart.add(product)
+    return redirect('store')
+
+
+def remove_product(request, product_id):
+    shopping_cart = ShoppingCart(request)
+    product = Product.objects.get(id=product_id)
+    shopping_cart.remove(product)
+    return redirect('store')
+
+
+def decrement_product(request, product_id):
+    shopping_cart = ShoppingCart(request)
+    product = Product.objects.get(id=product_id)
+    shopping_cart.decrement(product)
+    return redirect('store')
+
+
+def clear_cart(request):
+    shopping_cart = ShoppingCart(request)
+    shopping_cart.clear()
+    return redirect('store')
